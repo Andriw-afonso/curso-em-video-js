b5f9e963cdb3cc218f128b863dc307b4aac31710
Andriw-afonso <andriw_eng@outlook.com>
2023-02-28 10:45:32 -0300

    hgvghcvhgcvh

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+id (vel > 60){ // Condição simples
+    console.log("Você ultrapassou a velocídade permitida, MULTADO")
+} 
+console.log("Diríja usando sinto de segurança...").exit

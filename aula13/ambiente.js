dfdb2ea6d10ab83a8ae53f8b2f746970e85735a2
Andriw-afonso <andriw_eng@outlook.com>
2023-03-01 11:19:17 -0300

    bvjghfjgfhgfhg

---
+++ aula13/ambiente.js
@@ -0,0 +1,19 @@
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+
+var contador = 1
+
+while (contador < 6){
+    console.log(`Stage ${contador}`)
+    contador++
+}
+
+var contador = 1
+do {
+    console.log(`Stage ${contador}`)
+    contador++
+} while (contador < 6)

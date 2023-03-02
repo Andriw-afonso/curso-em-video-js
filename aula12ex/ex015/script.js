20d72d5d0947451ff007f16dc6503c923f34e7ef
Andriw-afonso <andriw_eng@outlook.com>
2023-03-02 13:19:30 -0300

    khbjhbjhbjhbjh

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -10,12 +10,41 @@ function verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
     }
     if (fsex[0].checked){
-        genero = 'Homen'
+        genero = 'Homem'
+        if (idade >= 0 && idade < 10){
+            //crianca
+            img.setAttribute('src', 'crianca-homen.jpg')
+        } else if (idade < 21){
+            //jovem
+            img.setAttribute('src', 'jovem-homen.jpg')
+        } else if (idade < 50){
+            //adulto
+            img.setAttribute('src', 'homen.jpg')
+        } else{
+            //idoso
+            img.setAttribute('src', 'idoso-homen.jpg')
+        }
     } else if (fsex[1].checked){
         genero = 'Mulher'
+        if (idade >= 0 && idade < 10){
+            //crianca
+            img.setAttribute('src', 'crianca-mulher.jpg')
+        } else if (idade < 21){
+            //jovem
+            img.setAttribute('src', 'jovem-mulher.jpg')
+        } else if (idade < 50){
+            //adulto
+            img.setAttribute('src', 'mulher.jpg')
+        } else{
+            //idoso
+            img.setAttribute('src', 'idosa-mulher.jpg')
+        }
     }
-    res.innerHTML = `Detectamos ${genero} com ${idade} anos.
-    `
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    res.appendChild(img)
 }

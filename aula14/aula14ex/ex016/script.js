e758f5d7d5804bf6aabcf3d3e6d249953941cc91
Andriw-afonso <andriw_eng@outlook.com>
2023-03-03 10:19:45 -0300

    kjhgjhgjhvjghv

--- aula14/aula14ex/ex016/script.js
+++ aula14/aula14ex/ex016/script.js
@@ -4,9 +4,38 @@ function contar(){
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
 
+    
     if (ini.value.length == 0 || fim.value.length ==0 || passo.value.length == 0){
-        window.alert('[ERRO] faltam dados !')
+        res.innerHTML = "Impossível contar !"
+        //window.alert('[ERRO] faltam dados !')
+        
     } else {
-        res.innerHTML = 'Contando...'
+        res.innerHTML = 'Contando... <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (p <= 0){
+            window.alert('Passo inválido !')
+        }
+
+        if (i < f){
+            // Contagem crescente
+            for(let c = i; c <= f ; c += p){
+                res.innerHTML += `${c}  \u{1F449}`
+            }
+            
+        } else {
+            // Contagem decrescente
+            for (let c = i ; c >=f ; c -= p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
+        
+
+    
+    
 }
